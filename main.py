e4a5cae329d7f3519be065fde6ff3e30344ad753
Yuta Hinokuma <yuta@abeja.asia>
2015-06-21 23:23:45 +0900

    複数のページのレイアウトに対応

--- main.py
+++ main.py
@@ -26,28 +26,49 @@ find_review_anchers = _\
     .find_element_by_id("histogramTable")\
     .find_elements_by_tag_name("a")
 
+def or_pipe(x, *args, **kwargs):
+    for arg in args:
+        t = arg(x)
+        if t:
+            return t
+    return kwargs.get("default", None)
+
 def get_reviews(x):
     return pipe(x, 
-        _.find_elements_by_class_name("reviewText"),
+        lambda x: or_pipe(x, 
+            _.find_elements_by_class_name("reviewText"),
+            _.find_elements_by_class_name("review-text"),
+            default=[],
+        ),
         map(lambda x: x.text)
     )
 
 def go_review_page(rank):
+    def test(x):
+        print x.tag_name
+        return x
+
     return lambda x: pipe(x,
         find_review_anchers,
         _[rank],
+        test,
         _.click(),
     )
 
 def next_page(page):
     try:
         return next(page.execute_page_transition_yield(
-            _.find_element_by_class_name("CMpaginate"),
+            lambda x: or_pipe(x,
+                _.find_elements_by_class_name("CMpaginate"),
+                _.find_elements_by_class_name("a-last"),
+                default=[]
+            ),
+            _[0],
             _.find_elements_by_tag_name("a"),
             filter(lambda x: u"次" in x.text),
             list,
         )(_.click()))
-    except StopIteration:
+    except Exception:
         return None
 
 def fetch_reviews(page, rank):
@@ -67,7 +88,7 @@ def fetch_reviews_detail(page):
         ) + fetch_reviews_detail(next_page(page))
     return reviews
 
-def main(product_id):
+def main(product_id, mode):
 
     url = "http://www.amazon.co.jp/gp/product/{}/".format(product_id)
 
@@ -75,20 +96,25 @@ def main(product_id):
         print top.execute(
             find_review_anchers,
         )
-        print "positive(star 5)"
-        positive_review = fetch_reviews(top, 0)
-        save_reviews(product_id, "p", positive_review)
+        if "p" in mode:
+            print "positive(star 5)"
+            positive_review = fetch_reviews(top, 0)
+            save_reviews(product_id, "p", positive_review)
 
-        print "even (star 3)"
-        even_reviews = fetch_reviews(top, 2)
-        save_reviews(product_id, "e", even_reviews)
+        if "e" in mode:
+            print "even (star 3)"
+            even_reviews = fetch_reviews(top, 2 * 3)
+            save_reviews(product_id, "e", even_reviews)
 
-        print "negative(star 1)"
-        even_reviews = fetch_reviews(top, 4)
-        save_reviews(product_id, "n", even_reviews)
+        if "n" in mode:
+            print "negative(star 1)"
+            negative_reviews = fetch_reviews(top, 4 * 3)
+            save_reviews(product_id, "n", negative_reviews)
 
         
 if __name__ == '__main__':
     sys.argv.append("4167732017")
+    sys.argv.append("pen")
     url = sys.argv[1]
-    main(url)
+    mode = "pen"
+    main(url, mode)
